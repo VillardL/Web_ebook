4590c65b45635983e32c94845f2add89ee23f11e
villard <1142356301@qq.com>
2019-03-15 23:38:22 +0800

    week3_partly

---
+++ week3/JS/index.js
@@ -0,0 +1,6 @@
+function Cr(fcolor) {
+    document.getElementsByClassName("black1").style.color=fcolor;
+}
+function docu(document1) {
+    document.getElementsByClassName("doc").style.document=document1;
+}
